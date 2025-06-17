1e1cae0e5608151b135094b9d7d77b2a177bdbd2
bw441093 <bw441093@greendreamteam.onmicrosoft.com>
2025-06-17 21:16:52 +0300

    Update db.ts

--- server/db/db.ts
+++ server/db/db.ts
@@ -3,7 +3,7 @@ import { drizzle, NodePgDatabase } from 'drizzle-orm/node-postgres';
 import * as schema from './schema';
 
 export const db: NodePgDatabase<typeof schema> = drizzle(
-	'postgres://postgres:secret@localhost:5432/pms',
+	process.env.DB_URI || 'postgres://postgres:secret@localhost:5432/pms',
 	{
 		schema,
 	}
